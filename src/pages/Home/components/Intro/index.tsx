a71ea09e4e55f2dd2f72b8cf01e5ab0f166368c8
unknown <gabrielbld9@gmail.com>
2022-12-07 09:00:40 -0300

    feat: componente onde recebe o outro componente infoWithIcon

---
+++ src/pages/Home/components/Intro/index.tsx
@@ -0,0 +1,55 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react';
+import { useTheme } from 'styled-components';
+
+import coffee from '../../../../assets/coffee.svg';
+import { InfoWithIcon } from '../../../../components/InfoWithIcon';
+import { RegularText } from '../../../../components/Typography';
+import {
+  BenefitsContainer,
+  IntroContainer,
+  IntroContent,
+  IntroTitle,
+} from './styles';
+export function Intro() {
+  const { colors } = useTheme();
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <div>
+          <section>
+            <IntroTitle size="xl">
+              Encontre o café perfeito para qualquer hora do dia
+            </IntroTitle>
+            <RegularText size="l" color="subtitle">
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </RegularText>
+          </section>
+          <BenefitsContainer>
+            <InfoWithIcon
+              iconBg={colors['brand-yellow-dark']}
+              icon={<ShoppingCart weight="fill" />}
+              text="Compra simples e segura"
+            />
+            <InfoWithIcon
+              iconBg={colors['base-text']}
+              icon={<Package weight="fill" />}
+              text="Embalagem mantém o café intacto"
+            />
+            <InfoWithIcon
+              iconBg={colors['brand-yellow']}
+              icon={<Timer weight="fill" />}
+              text="Entrega rápida e rastreada"
+            />
+            <InfoWithIcon
+              iconBg={colors['brand-purple']}
+              icon={<Coffee weight="fill" />}
+              text="O café chega fresquinho até você"
+            />
+          </BenefitsContainer>
+        </div>
+        <img src={coffee} alt="Logo" />
+      </IntroContent>
+    </IntroContainer>
+  );
+}
