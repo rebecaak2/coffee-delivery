a243f6c29bd471886bac2ff822682d937fdb1616
unknown <gabrielbld9@gmail.com>
2022-12-07 09:01:26 -0300

    style: css do intro

---
+++ src/pages/Home/components/Intro/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+import { TitleText } from '../../../../components/Typography';
+
+export const IntroContainer = styled.section`
+  width: 100%;
+  height: 34rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const IntroContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.5rem;
+`;
+
+export const IntroTitle = styled(TitleText)`
+  margin-bottom: 1rem;
+`;
+
+export const BenefitsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+  margin-top: 4.125rem;
+`;
